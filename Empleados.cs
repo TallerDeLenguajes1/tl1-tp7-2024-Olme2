1c74dd6b7056e1f41e0b28a94f6690670115f46b
Valentino Olmedo <valenolme@gmail.com>
2024-06-25 16:10:54 -0300

    Commit empleados 1

---
+++ Empleados.cs
@@ -0,0 +1,11 @@
+namespace Empleados{
+    public class Empleado{
+        public string nombre;
+        public string apellido;
+        public DateTime nacimiento;
+        public char estadoCivil;
+        public DateTime ingreso;
+        public double sueldo;
+        
+    }
+}
