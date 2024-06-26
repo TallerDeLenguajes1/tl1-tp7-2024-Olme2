fcc2340025b35237523654433a92167123f6b085
Valentino Olmedo <valenolme@gmail.com>
2024-06-26 09:47:49 -0300

    Commit empleados 2

--- Empleados.cs
+++ Empleados.cs
@@ -1,11 +1,61 @@
 namespace Empleados{
+    public enum Cargos{
+        Auxiliar,
+        Administrativo,
+        Ingeniero,
+        Especialista,
+        Investigador
+    }
     public class Empleado{
-        public string nombre;
-        public string apellido;
-        public DateTime nacimiento;
-        public char estadoCivil;
-        public DateTime ingreso;
-        public double sueldo;
-        
+        public required string Nombre;
+        public required string Apellido;
+        public DateTime Nacimiento;
+        public char EstadoCivil;
+        public DateTime Ingreso;
+        public double Sueldo;
+        public Cargos Cargo;
+        public int calcularAntiguedad(){
+            DateTime fechaActual=DateTime.Now;
+            int añosAntiguedad=fechaActual.Year-this.Ingreso.Year;
+            if (this.Ingreso.Date > fechaActual.AddYears(-añosAntiguedad)){
+                añosAntiguedad--;
+            }
+            return añosAntiguedad;
+        }
+        public int calcularEdad(){
+            DateTime fechaActual=DateTime.Today;
+            int edad=fechaActual.Year-this.Nacimiento.Year;
+            if(this.Nacimiento.Date>fechaActual.AddYears(-edad)){
+                edad--;
+            }
+            return edad;
+        }
+        public int calcularJubilacion(){
+            int edad=calcularEdad();
+            if(edad<65){
+                int tiempoParaJubilarse=65-edad;
+                return tiempoParaJubilarse;
+            }else{
+                return 0;
+            }
+        }
+        public double calcularSalario(){
+            int antiguedad=calcularAntiguedad();
+            double Salario;
+            double adicional;
+            if(antiguedad<20){
+                adicional=(0.01*this.Sueldo)*antiguedad;
+            }else{
+                adicional=this.Sueldo*0.25;
+            }
+            if(this.Cargo==Cargos.Ingeniero || this.Cargo==Cargos.Especialista){
+                adicional*=1.5;
+            }
+            if(this.EstadoCivil=='c'){
+                adicional+=150000;
+            }
+            Salario=this.Sueldo+adicional;
+            return Salario;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using Empleados;
+Empleado[] empleados=new Empleado[3];
+empleados[0]=new Empleado{
+    Nombre="Jose",
+    Apellido="Ramirez",
+    Nacimiento=new DateTime(1990, 7, 20),
+    EstadoCivil='c',
+    Ingreso=new DateTime(2005,9,11),
+    Sueldo=254000,
+    Cargo=Cargos.Auxiliar
+};
+empleados[1]=new Empleado{
+    Nombre="Micaela",
+    Apellido="Noli Moyano",
+    Nacimiento=new DateTime(2004, 3, 23),
+    EstadoCivil='s',
+    Ingreso=new DateTime(2018,03,30),
+    Sueldo=254000,
+    Cargo=Cargos.Especialista
+};
+empleados[2]=new Empleado{
+    Nombre="Valentino",
+    Apellido="Olmedo",
+    Nacimiento=new DateTime(2003, 12, 26),
+    EstadoCivil='c',
+    Ingreso=new DateTime(2012,05,23),
+    Sueldo=254000,
+    Cargo=Cargos.Ingeniero
+};
+double total=0;
+int i=0;
+int guardar=0;
+int menor=1000;
+foreach(var empleado in empleados){
+    total+=empleado.calcularSalario();
+    if(empleado.calcularJubilacion()<menor){
+       menor=empleado.calcularJubilacion();
+       guardar=i;
+    }
+    i++;
+}
+Console.WriteLine($"TOTAL A PAGAR EN CONCEPTO DE SALARIOS:${total:N2}");
+Console.WriteLine($"\n||EMPLEADO MAS CERCANO A JUBILARSE||\nEMPLEADO:{empleados[guardar].Nombre} {empleados[guardar].Apellido}\nFECHA DE NACIMIENTO:{empleados[guardar].Nacimiento.Day}/{empleados[guardar].Nacimiento.Month}/{empleados[guardar].Nacimiento.Year}\nESTADO CIVIL:{empleados[guardar].EstadoCivil}\nINGRESO A LA EMPRESA:{empleados[guardar].Ingreso.Day}/{empleados[guardar].Ingreso.Month}/{empleados[guardar].Ingreso.Year}\nSUELDO:${empleados[guardar].Sueldo:N2}\nCARGO:{empleados[guardar].Cargo}\nSALARIO FINAL:${empleados[guardar].calcularSalario():N2}\nTIEMPO PARA JUBILARSE:{empleados[guardar].calcularJubilacion()} años\nANTIGUEDAD EN LA EMPRESA:{empleados[guardar].calcularAntiguedad()} años\nEDAD:{empleados[guardar].calcularEdad()} años");
